5b3561883b3688a537480b44e75e80cfc98f0842
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-19 10:50:32 +0800

    tree in c++

---
+++ tree.cpp
@@ -0,0 +1,30 @@
+struct Node
+{
+    int date;
+    Node *left,*right;
+    Node(){date=0;left=right=0;}
+    Node(int d){date=d;left=right=0;}
+};
+Node a,*p=new Node;
+Node X();
+int main(){
+    Node X1();
+    Node X2();
+    Node X1();
+
+}
+// Node::Node(){
+//     date=0;
+//     left=right=0;
+// }
+// Node::Node(int _date){
+//     date=_date;
+//     left=right=0;
+// }
+// int main(){
+//     Node *tree=0;
+//     tree=new Node(1);
+//     tree->left=new Node(2);
+//     tree->right=new Node(3);
+// }
+
