1456a3ee2959e9f0a143af8849386a2550f6ea4c
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-20 23:15:15 +0800

    mid 1 end

--- mid1/1.c
+++ mid1/1.c
@@ -1,18 +1,36 @@
 #include<stdio.h>
+// int chBig(char* str,int n){
+//     int num=0,i=0;
+//     while(i<n){
+//         if(*(str+i)>='A' && *(str+i)<='Z')
+//             num+=1;
+//         i+=1;
+//     }
+//     return num;
+// }
+
+// int chBig(char* str,int n){
+//     int num=0;
+//     for(int i=0;i<n;i++){
+//         if(*(str+i)>='A' && *(str+i)<='Z')
+//             num+=1;
+//     }
+//     return num;
+// }
+
 int chBig(char* str,int n){
-    int num=0,i=0;
-    while(i<n){
-        if(*(str+i)>='A' && *(str+i)<='Z')
-            num+=1;
-        i+=1;
-    }
-    return num;
+    if(*(str)=='\0')
+        return 0;
+    if(*(str)>='A' && *(str)<='Z')
+        return chBig(str+1,n)+1;
+    else
+        return chBig(str+1,n);
 }
 
 int main(){
     /*char str[100];
     fgets(str,100,stdin);*/
-    char str[]="AABB12345avads";
+    char str[]="AAAA123456afdadA";
     int n=sizeof(str)/sizeof(str[0]);
     if(n==0)
         printf("%d",n);
