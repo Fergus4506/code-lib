9029467d230bf3b76a18dbfe264f71d7a09cfff9
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-20 22:35:34 +0800

    1.1.1

---
+++ mid1/1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int chBig(char* str,int n){
+    int num=0,i=0;
+    while(i<n){
+        if(*(str+i)>='A' && *(str)<='Z')
+            num+=1;
+        i+=1;
+    }
+    return num;
+}
+
+int main(){
+    char str[100];
+    fgets(str,100,stdin);
+    int n=sizeof(str)/sizeof(str[0]);
+    if(n==0)
+        printf("%d",n);
+    else{
+        int ans=chBig(&str[0],n);
+        printf("%d",ans);
+    }
+    return 0;
+}
